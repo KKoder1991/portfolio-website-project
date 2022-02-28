004ba7a91ea6bb0e723c8decad58b13b70e44062
KKoder1991 <konondlovu@icloud.com>
2022-02-28 14:23:52 +0100

    add all info to map to projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,10 +21,21 @@ export default function Projects(){
                         href={project.link}
                         key={project.image}
                         className="sm:w-1/2 w-100 p-4">
-                            <img alt="gallery"
-                                className="absolute inset-0 w-full h-full object-cover object-center"
-                                src={project.image}
+                            <div className="flex relative">
+                                <img alt="gallery"
+                                    className="absolute inset-0 w-full h-full object-cover object-center"
+                                    src={project.image}
                                 />
+                                <div className="px-8 py-10 relative z-10 w-full border-4 bordergray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                                    <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                                        {project.subtitle}
+                                    </h2>
+                                    <h1 className="title-font text-lg font-medium text-white mb-3">
+                                        {project.title}
+                                    </h1>
+                                    <p className="leading-relaxed">{project.description}</p>
+                                </div>
+                            </div>
                         </a>
                 ))}
             </div>
