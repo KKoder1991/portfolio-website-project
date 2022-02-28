36bc4935688673d2b2621fa3fb70f3c89086cb31
KKoder1991 <konondlovu@icloud.com>
2022-02-28 11:55:13 +0100

    remove duplicate Contacts import statement

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./components/NavBar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
-import Contact from "./components/Contact";
 
 
 export default function App(){
