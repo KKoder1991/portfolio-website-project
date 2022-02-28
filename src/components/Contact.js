d57c60d4c2d694437613effe5a70afe8eba4b164
KKoder1991 <konondlovu@icloud.com>
2022-02-28 17:04:46 +0100

    Add message area and submit button for contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -43,7 +43,33 @@ export default function Contact(){
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
                         suscipit officia aspernatur veritatis. Asperiores, aliquid?
                     </p>
-                    
+                    <div className="relative mb-4">
+                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                            Name
+                        </label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            />
+                        </label>
+                    </div>
+                    <button 
+                        type="submit"
+                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                        >
+                            Submit
+                        </button>
                     </form>
             </div>
         </section>
