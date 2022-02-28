04269b215e64b84499c0b3f840aff1d2373ac6f2
KKoder1991 <konondlovu@icloud.com>
2022-02-28 13:59:42 +0100

    create sample data for map to Projects comp

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,23 @@
+import { CodeIcon } from '@heroicons/react/solid';
+import React from "react";
+import { projects } from "..data";
+
 export default function Projects(){
-    
+    return (
+        <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+            <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+                <CodeIcon className="mx-auto inline-block w-1- mb-4" />
+                <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+                    Apps I've Built
+                </h1>
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                    These are some of the applications that I have built, 
+                    mainly using JavaScript, React as a frameworok and using programming logic.
+                </p>
+            </div>
+            <div className="flex flex-wrap -m-4">
+                
+            </div>
+        </section>
+    )
 }
---
+++ src/components/data.js
@@ -0,0 +1,10 @@
+const projects = [
+    {
+        title: "CapSol",
+        subtitle: "JavaScript, React, MySQL",
+        description:
+          "React was a university project, it is meant to connect users with capsules created by other users filled with art, music and recordings.",
+        image: "./project-2.gif",
+        link: "https://reedbarger.com",
+    }
+]
